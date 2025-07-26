295105c25c344093a7d244dae2e05038da017573
Geekhubbs <owusuamohsamuel@gmail.com>
2025-07-26 20:37:15 +0000

    Fixed Url

--- constants/config.js
+++ constants/config.js
@@ -3,7 +3,7 @@ export const BACKEND_URL = 'http://10.132.202.162:8080';
 
 // API endpoints
 export const API_ENDPOINTS = {
-  REGISTER: `${BACKEND_URL}/user/register`,
-  LOGIN: `${BACKEND_URL}/user/login`,
+  REGISTER: `${BACKEND_URL}/auth/register`,
+  LOGIN: `${BACKEND_URL}/auth/login`,
   // Add more endpoints as needed
 }; 
